0f225918bd9821ac212e4604cc64ed631118d1b3
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-03-09 01:16:37 +0530

    "initials"

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initials="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0
+    gradient-01" />
+  </motion.nav>
+);
+export default Navbar;
