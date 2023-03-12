a9582fc213b3d4fc78bbe79aa4e342bf11cd6094
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-03-12 13:48:30 +0530

    explore completed

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -6,7 +6,7 @@ import { textContainer, textVariant2 } from '../utils/motion';
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={`font-normal text-[14px]
+    className={`font-normal text-[20px]
     text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
@@ -19,6 +19,14 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px]
+     text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
